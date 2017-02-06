6096819392872cd85442477121fd9efcda869894
Paul Friedrich <pfriedrich123@outlook.com>
2017-02-06 17:39:47 +0100

    added terminator config; updated i3 config

--- .config/i3/i3py.py
+++ .config/i3/i3py.py
@@ -1,10 +1,12 @@
-import i3pystatus
+from i3pystatus import Status
+from i3pystatus.weather import weathercom
+from i3pystatus import parcel
 
-status = i3pystatus.core.Status()
+status = Status()
 
 status.register("clock", format="  %a %F  %T",)
 
-status.register("dpms", format="", color_disabled="#A52A2A")
+status.register("dpms", format="", format_disabled="", color_disabled="#A52A2A")
 
 status.register("pulseaudio", format=" {volume}", multi_colors=True)
 
@@ -16,13 +18,15 @@ status.register("temp")
 
 status.register("network", interface="enp0s25",
                 format_up=" {bytes_sent} MiB/s  {bytes_recv} MiB/s",
-                divisor=1024**2)
+                divisor=1024**2, recv_limit=100000, sent_limit=40000)
+
+status.register("xkblayout", layouts=["de", "us colemak"])
 
 status.register("disk", format=" {avail} GiB",
                 path="/")
 
-status.register("weather", location_code="GMXX0063", colorize=True,
-                format="{current_temp}")
+status.register("weather", backend=weathercom.Weathercom(location_code="GMXX4347:1:GM", units="metric"), colorize=True,
+                format="{current_temp}°C")
 
 status.register("spotify", format=" {status} {artist} - {title}",
                 format_not_running="")
